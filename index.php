28aba49515ef73a5db4aacd7656b66956b4f2ba4
komputronika <piepin@gmail.com>
2019-03-20 11:36:16 +0700

    Upload form

--- index.php
+++ index.php
@@ -10,6 +10,21 @@
     body {
         margin: 1rem;
     }    
+    .submit {
+        margin-top: 1rem;
+    }
+    .container {
+        padding-right:0;
+        padding-left:0;
+    }
+
+    h3 {
+        margin-bottom: 0.0 !important;
+    }
+
+    #convert-form {
+        font-size: 120%;
+    }
     </style>    
 
 </head>
@@ -20,13 +35,54 @@
             <div class="col-12">
 
                 <div class="card">
+                    <div class="card-header">
+                        <h2 class="card-title font-weight-bold">MYSQL SQL Converter</h2>
+                        <h3 class="card-subtitle mb-2 text-muted">From Excel (.xls/.xlsx) file</h4>
+                      </div>
                     <div class="card-body">
-                        <h5 class="card-title">MYSQL SQL Generator</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">From Excel (.xls/.xlsx) file</h6>
+                        
                         <p class="card-text">
+
+<form id="convert-form" method="POST" action="convert.php">
+  <div class="form-group">
+    <label for="exampleFormControlFile1"><b>Choose Excel file</b></label>
+    <input type="file" accept=".xls,.xlsx" class="form-control-file" id="exampleFormControlFile1">
+  </div>
+
+
+  <b>Time column</b>
+  <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+      <label class="form-check-label" for="defaultCheck1">
+        Automatically add: created_ad, updated_at, deleted_at, is_deleted
+      </label>
+  </div>
+<br/>
+
+<b>Type for ID Column</b>
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+  <label class="form-check-label" for="exampleRadios1">
+    Unsigned Integer Auto Increment
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+  <label class="form-check-label" for="exampleRadios2">
+    UUID() 
+  </label>
+</div>
+
+<div class="submit">
+  <button type="submit" class="btn btn-primary">Convert</button>
+</div>    
+
+</form>
+
+
+
                         </p>
-                        <a href="#" class="card-link">Upload</a>
-                        <a href="#" class="card-link">Save</a>
+                        
                   </div>
                 </div>
             </div>
