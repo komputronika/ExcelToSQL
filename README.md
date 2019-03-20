2822d1837f63f4267c05a621c5c2b4f7b7df16b4
komputronika <piepin@gmail.com>
2019-03-20 10:26:03 +0700

    Update

---
+++ README.md
@@ -0,0 +1,3 @@
+# MySQL SQL Generator from Excel File
+
+In development, not ready to use.
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE doctype html>
+<html lang="en">
+<head>
+    <title>Excel to MySQL Converter</title>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+    <link crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" rel="stylesheet">
+
+    <style>
+    body {
+        margin: 1rem;
+    }    
+    </style>    
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">MYSQL SQL Generator</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">From Excel (.xls/.xlsx) file</h6>
+                        <p class="card-text">
+                        </p>
+                        <a href="#" class="card-link">Upload</a>
+                        <a href="#" class="card-link">Save</a>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script crossorigin="anonymous" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" src="https://code.jquery.com/jquery-3.3.1.slim.min.js">
+    </script>
+    <script crossorigin="anonymous" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js">
+    </script>
+    <script crossorigin="anonymous" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js">
+    </script>
+</body>
+</html>
