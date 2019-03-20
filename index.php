4437af368783a8aa94ad9e84969bd985533fb944
komputronika <piepin@gmail.com>
2019-03-20 14:46:09 +0700

    Convert Excel

--- index.php
+++ index.php
@@ -22,14 +22,52 @@
         margin-bottom: 0.0 !important;
     }
 
-    #convert-form {
+    /* #convert-form 
+    {
         font-size: 120%;
-    }
+    } */
+    
     </style>    
 
 </head>
 
 <body>
+
+<?php
+
+$regions = array(
+    'Africa' => DateTimeZone::AFRICA,
+    'America' => DateTimeZone::AMERICA,
+    'Antarctica' => DateTimeZone::ANTARCTICA,
+    'Asia' => DateTimeZone::ASIA,
+    'Atlantic' => DateTimeZone::ATLANTIC,
+    'Europe' => DateTimeZone::EUROPE,
+    'Indian' => DateTimeZone::INDIAN,
+    'Pacific' => DateTimeZone::PACIFIC
+);
+$timezones = array();
+foreach ($regions as $name => $mask)
+{
+    $zones = DateTimeZone::listIdentifiers($mask);
+    
+    foreach($zones as $timezone)
+    {
+        $time = new DateTime(NULL, new DateTimeZone($timezone));
+        $hh = ceil($time->getOffset()/3600);
+        $mm = str_pad( abs($time->getOffset() % 3600)/60, 2, "0", STR_PAD_LEFT);
+
+        $offset = $hh.":".$mm;
+        if ($hh>0) $offset = "+".$offset;
+
+        // print "$timezone = ".$offset."<br>";
+
+        // $ampm = $time->format('H') > 12 ? ' ('. $time->format('g:i a'). ')' : '';
+        // $timezones[$name][$timezone] = substr($timezone, strlen($name) + 1) . ' - ' . $time->format('H:i') . $ampm;
+
+        $timezones[$name][$timezone] = $offset;
+    }
+}
+?>
     <div class="container">
         <div class="row">
             <div class="col-12">
@@ -43,38 +81,57 @@
                         
                         <p class="card-text">
 
-<form id="convert-form" method="POST" action="convert.php">
+<form id="convert-form" method="POST" action="convert.php" enctype="multipart/form-data">
   <div class="form-group">
-    <label for="exampleFormControlFile1"><b>Choose Excel file</b></label>
-    <input type="file" accept=".xls,.xlsx" class="form-control-file" id="exampleFormControlFile1">
+    <label for="file"><b>Choose Excel file</b></label>
+    <input type="file" accept=".xls,.xlsx" class="form-control-file" id="file" name="file">
   </div>
 
 
-  <b>Time column</b>
+  <b>Moment column</b>
   <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
-      <label class="form-check-label" for="defaultCheck1">
-        Automatically add: created_ad, updated_at, deleted_at, is_deleted
+      <input class="form-check-input" type="checkbox" value="1" id="moment" name="moment">
+      <label class="form-check-label" for="moment">
+        Automatically add column: created_at, updated_at, deleted_at, is_deleted
       </label>
   </div>
 <br/>
 
 <b>Type for ID Column</b>
 <div class="form-check">
-  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-  <label class="form-check-label" for="exampleRadios1">
-    Unsigned Integer Auto Increment
+  <input class="form-check-input" type="radio" name="idcol" id="idcol" value="ai" checked>
+  <label class="form-check-label" for="idcol">
+    Unsigned Integer - Auto Increment
   </label>
 </div>
 <div class="form-check">
-  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
-  <label class="form-check-label" for="exampleRadios2">
-    UUID() 
+  <input class="form-check-input" type="radio" name="idcol" id="idcol" value="uuid">
+  <label class="form-check-label" for="idcol">
+    UUID() - Auto generate
   </label>
 </div>
 
+<br/>
+<div class="form-group">
+      <label for="inlineFormCustomSelect"><b>Database Timezone</b></label>
+      <select class="form-control" id="timezone" name="timezone">
+<?php
+foreach($timezones as $region => $list)
+{
+    print '<optgroup label="' . $region . '">' . "\n";
+    foreach($list as $name => $timezone)
+    {
+        print '<option value="' . $timezone . '">' . $name . '</option>' . "\n";
+    }
+    print '<optgroup>' . "\n";
+}
+?>
+      </select>
+</div>
+
+
 <div class="submit">
-  <button type="submit" class="btn btn-primary">Convert</button>
+  <button type="submit" value="submit" name="submit" class="btn btn-primary">Convert</button>
 </div>    
 
 </form>
